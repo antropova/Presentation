ea88656b3d578c109402850d8ee9f1dfda89ac75
Maria (Masha) Antropova <masha_antropova@gmail.com>
2015-04-25 19:13:44 -0400

    Fix brand button

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,6 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_action :set_locale
-
-  def set_locale
-    I18n.locale ||= params[:locale]
-  end
 
   def change_locale
     I18n.locale = params[:locale]
