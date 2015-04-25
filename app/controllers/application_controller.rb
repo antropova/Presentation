32fb70d7973e6079ccb75039a6ad76604d05cde2
Andrei Motinga <motingandrei7@gmail.com>
2015-04-25 19:01:03 -0400

    views and english

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,14 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :set_locale
- 
+
   def set_locale
-    I18n.locale = params[:locale] || I18n.default_locale
+    I18n.locale ||= params[:locale]
   end
+
+  def change_locale
+    I18n.locale = params[:locale]
+    redirect_to root_path
+  end
+
 end
